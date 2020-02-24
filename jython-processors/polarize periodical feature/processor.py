7b67a05536988cc736def51510193c457bb95587
admin <admin>
2020-02-24 15:39:41 +0100

    Edited file 'jython-processors/polarize periodical feature/processor.py'

--- jython-processors/polarize periodical feature/processor.py
+++ jython-processors/polarize periodical feature/processor.py
@@ -3,16 +3,16 @@ def process(row):
     periodical_column_name = params.get('periodical_column')
     periodical_column_value = row[periodical_column_name]
     column_period = params.get('column_period')
-    arg = math.pi/2.0-(math.pi*int(periodical_column_value))/(column_period/2.0)
+    arg_val = math.pi/2.0-(math.pi*int(periodical_column_value))/(column_period/2.0)
     
     arg_name = periodical_column_name + "_arg"
     
     cos_name = periodical_column_name + "_cos"
     sin_name = periodical_column_name + "_sin"
     tan_name = periodical_column_name + "_tan"
-    cos_val = math.cos(arg)
-    sin_val = math.sin(arg)
-    tan_val = math.tan(arg)
+    cos_val = math.cos(arg_val)
+    sin_val = math.sin(arg_val)
+    tan_val = math.tan(arg_val)
     
     res = {arg_name:arg_val, cos_name:cos_val, sin_name:sin_val, tan_name:tan_val, }
     
