6be9f8af2ab915e15862da2396dd0a2f2f54e98e
admin <admin>
2020-02-24 15:18:22 +0100

    Edited file 'jython-processors/polarize periodical feature/processor.py'

--- jython-processors/polarize periodical feature/processor.py
+++ jython-processors/polarize periodical feature/processor.py
@@ -1,7 +1,7 @@
 def process(row):
     import math
     periodical_column_name = params.get('periodical_column')
-    periodical_column_value = row['periodical_column_name']
+    periodical_column_value = row[periodical_column_name]
     column_period = params.get('column_period')
     arg = math.pi/2.0-(math.pi*int(periodical_column_value))/(column_period/2.0)
     
