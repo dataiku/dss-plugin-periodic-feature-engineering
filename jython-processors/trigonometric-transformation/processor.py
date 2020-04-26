8e2c1756c6e44861a32575ddad5fa14a5465b1a4
admin <admin>
2020-04-26 18:02:17 +0200

    Edited files

--- jython-processors/trigonometric-transformation/processor.py
+++ jython-processors/trigonometric-transformation/processor.py
@@ -9,25 +9,21 @@ def process(row):
     compute_sin = params.get('compute_sin')
     compute_tan = params.get('compute_tan')
     output_arg = params.get('output_arg')
-
-    periodical_column_value = row[periodical_column_name]
     
+    #Production code :
+    periodical_column_value = row[periodical_column_name]
     arg_val = math.pi/2.0-(math.pi*int(periodical_column_value))/(column_period/2.0)
     res = {}
     
-    
     if compute_cos:
         cos_name = periodical_column_name + "_cos"
-        res[cos_name] = math.cos(arg_val)
-         
+        res[cos_name] = math.cos(arg_val)   
     if compute_sin:
         sin_name = periodical_column_name + "_sin"
         res[sin_name] = math.sin(arg_val)
-  
     if compute_tan:
         tan_name = periodical_column_name + "_tan"
         res[tan_name] = math.tan(arg_val)
-        
     if output_arg:
         arg_name = periodical_column_name + "_arg"
         res[arg_name] = arg_val
