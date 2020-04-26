bfabad49b09e12c929810d60cff51c717e4ed411
admin <admin>
2020-04-26 18:01:18 +0200

    Edited files

--- jython-processors/trigonometric-transformation/processor.py
+++ jython-processors/trigonometric-transformation/processor.py
@@ -4,19 +4,32 @@ import json
 def process(row):
     
     periodical_column_name = params.get('periodical_column')
-    periodical_column_value = row[periodical_column_name]
     column_period = params.get('column_period')
+    compute_cos = params.get('compute_cos')
+    compute_sin = params.get('compute_sin')
+    compute_tan = params.get('compute_tan')
+    output_arg = params.get('output_arg')
+
+    periodical_column_value = row[periodical_column_name]
+    
     arg_val = math.pi/2.0-(math.pi*int(periodical_column_value))/(column_period/2.0)
+    res = {}
     
-    arg_name = periodical_column_name + "_arg"
     
-    cos_name = periodical_column_name + "_cos"
-    sin_name = periodical_column_name + "_sin"
-    tan_name = periodical_column_name + "_tan"
-    cos_val = math.cos(arg_val)
-    sin_val = math.sin(arg_val)
-    tan_val = math.tan(arg_val)
-    
-    res = {arg_name:arg_val, cos_name:cos_val, sin_name:sin_val, tan_name:tan_val}
+    if compute_cos:
+        cos_name = periodical_column_name + "_cos"
+        res[cos_name] = math.cos(arg_val)
+         
+    if compute_sin:
+        sin_name = periodical_column_name + "_sin"
+        res[sin_name] = math.sin(arg_val)
+  
+    if compute_tan:
+        tan_name = periodical_column_name + "_tan"
+        res[tan_name] = math.tan(arg_val)
+        
+    if output_arg:
+        arg_name = periodical_column_name + "_arg"
+        res[arg_name] = arg_val
     
     return json.dumps(res)
