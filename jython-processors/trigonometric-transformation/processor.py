3adaa5c3a725a49d6b5acb18b387179538f4c16f
admin <admin>
2020-06-21 21:21:37 +0200

    Edited file 'jython-processors/trigonometric-transformation/processor.py'

--- jython-processors/trigonometric-transformation/processor.py
+++ jython-processors/trigonometric-transformation/processor.py
@@ -1,11 +1,18 @@
 import math
 import json
 
+
+class TrigonometricTransformer():
+    
+    def __init__(self)
+
 def compute_trigonometric_features(L_computation, arg):
     compute_cos = L_computation[0]
     compute_sin = L_computation[1]
     compute_tan = L_computation[2]
-    
+    if compute_cos:
+        cos_val = math.cos(arg_val)
+    else
     
 
 def process(row):
