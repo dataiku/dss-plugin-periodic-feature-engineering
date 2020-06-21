1d9b0147f73d7d2268d7af5db9bd778b3d352804
admin <admin>
2020-06-21 20:41:20 +0200

    Edited file 'jython-processors/trigonometric-transformation/processor.py'

--- jython-processors/trigonometric-transformation/processor.py
+++ jython-processors/trigonometric-transformation/processor.py
@@ -17,16 +17,16 @@ def process(row):
     try:
         if compute_cos:
             cos_name = periodical_column_name + "_cos"
-            dict_trigonometric_features[cos_name] = math.cos(arg_val)   
+            row[cos_name] = math.cos(arg_val)   
         if compute_sin:
             sin_name = periodical_column_name + "_sin"
-            dict_trigonometric_features[sin_name] = math.sin(arg_val)
+            row[sin_name] = math.sin(arg_val)
         if compute_tan:
             tan_name = periodical_column_name + "_tan"
-            dict_trigonometric_features[tan_name] = math.tan(arg_val)
+            row[tan_name] = math.tan(arg_val)
         if output_arg:
             arg_name = periodical_column_name + "_arg"
-            dict_trigonometric_features[arg_name] = arg_val
+            row[arg_name] = arg_val
         error_message = ""
     except Exception as e:
         error_message = str(e)
