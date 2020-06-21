954c8ce0efe4f8e39856d2130a434c9a3b37b0df
admin <admin>
2020-06-21 21:23:56 +0200

    Edited file 'jython-processors/trigonometric-transformation/processor.py'

--- jython-processors/trigonometric-transformation/processor.py
+++ jython-processors/trigonometric-transformation/processor.py
@@ -24,7 +24,7 @@ def process(row):
     #Parameters import :
     periodical_column_name = params.get('periodical_column')
     column_period = params.get('column_period')
-    compute_cos = params.get('compute_cos')
+    compute_cos = trigonometric_transformer.compute_cos
     compute_sin = params.get('compute_sin')
     compute_tan = params.get('compute_tan')
     output_arg = params.get('output_arg')
