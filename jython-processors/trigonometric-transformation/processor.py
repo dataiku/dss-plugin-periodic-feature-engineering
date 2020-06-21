9d12b976bf6460f61cb29c46d58241c76b2f6ade
admin <admin>
2020-06-21 23:03:20 +0200

    Edited file 'jython-processors/trigonometric-transformation/processor.py'

--- jython-processors/trigonometric-transformation/processor.py
+++ jython-processors/trigonometric-transformation/processor.py
@@ -47,7 +47,7 @@ trigonometric_transformer = TrigonometricTransformer(L_choosed_functions=L_choos
 def process(row):
     periodical_column_value = row[periodical_column_name]    
     try:
-        arg_val = math.pi/2.0-(math.pi*int(periodical_column_value))/(column_period/2.0)+math.pi
+        arg_val = math.pi/2.0-(math.pi*int(periodical_column_value))/(column_period/2.0)
         cos_val, sin_val, tan_val = trigonometric_transformer.compute_trigonometric_transform(arg_val)
         error_message = ""
         
