a86efb15db33c1b1e13a519cc1067a39ed54c2bf
alexcombessie <alex.combessie@gmail.com>
2020-09-15 14:15:04 +0200

    start refactoring

--- jython-processors/trigonometric-transformation/processor.py
+++ jython-processors/trigonometric-transformation/processor.py
@@ -1,8 +1,8 @@
-#Packages import
+# Packages import
 import math
 
-#Class definition :
-class PeriodicalFeaturesTransformer():
+
+class PeriodicalFeaturesTransformer:
     """
     Set up all the functions that will be called by the recipe's processor.
     These functions are defined as lambdas functions contextualized by the end user's selection.
@@ -30,10 +30,10 @@ class PeriodicalFeaturesTransformer():
         self.choosed_functions = choosed_functions
         self.period = period
         self.process_as_time_dimension = process_as_time_dimension
-        self.range_period = range(int(self.period+2))
+        self.range_period = range(int(self.period + 2))
         self.trigonometric_transformer = {}
         self.build_trigonometric_transformer(self.choosed_functions)
-        self.trigonometric_features = {'cos':{}, 'sin':{}, 'tan':{}}
+        self.trigonometric_features = {"cos": {}, "sin": {}, "tan": {}}
         if self.process_as_time_dimension:
             self.compute_trigonometric_features()
 
@@ -47,9 +47,11 @@ class PeriodicalFeaturesTransformer():
         cos_choosed = choosed_functions[0]
         sin_choosed = choosed_functions[1]
         tan_choosed = choosed_functions[2]
-        self.trigonometric_transformer = {'cos': lambda x: math.cos(x) if cos_choosed else None,
-                                          'sin': lambda x: math.sin(x) if sin_choosed else None,
-                                          'tan': lambda x: math.tan(x) if tan_choosed else None}
+        self.trigonometric_transformer = {
+            "cos": lambda x: math.cos(x) if cos_choosed else None,
+            "sin": lambda x: math.sin(x) if sin_choosed else None,
+            "tan": lambda x: math.tan(x) if tan_choosed else None,
+        }
         pass
 
     def compute_trigonometric_transform(self, argument):
@@ -60,9 +62,9 @@ class PeriodicalFeaturesTransformer():
         Args:
             - argument (float, mandatory) : A trigonometric argument.
         """
-        cos_val = self.trigonometric_transformer['cos'](argument)
-        sin_val = self.trigonometric_transformer['sin'](argument)
-        tan_val = self.trigonometric_transformer['tan'](argument)
+        cos_val = self.trigonometric_transformer["cos"](argument)
+        sin_val = self.trigonometric_transformer["sin"](argument)
+        tan_val = self.trigonometric_transformer["tan"](argument)
         return cos_val, sin_val, tan_val
 
     def compute_argument(self, value):
@@ -76,7 +78,7 @@ class PeriodicalFeaturesTransformer():
         Args:
             - value (float, mandatory) : The periodical feature's value.
         """
-        return math.pi/2.0-(math.pi*value)/(self.period/2.0)
+        return math.pi / 2.0 - (math.pi * value) / (self.period / 2.0)
 
     def compute_trigonometric_features(self):
         """
@@ -89,9 +91,9 @@ class PeriodicalFeaturesTransformer():
         """
         for value in self.range_period:
             value_argument = self.compute_argument(value)
-            self.trigonometric_features['cos'][value] = self.trigonometric_transformer['cos'](value_argument)
-            self.trigonometric_features['sin'][value] = self.trigonometric_transformer['sin'](value_argument)
-            self.trigonometric_features['tan'][value] = self.trigonometric_transformer['tan'](value_argument)
+            self.trigonometric_features["cos"][value] = self.trigonometric_transformer["cos"](value_argument)
+            self.trigonometric_features["sin"][value] = self.trigonometric_transformer["sin"](value_argument)
+            self.trigonometric_features["tan"][value] = self.trigonometric_transformer["tan"](value_argument)
             pass
 
 
@@ -121,9 +123,10 @@ def compute_trigonometric_labels(col_name, output_arg, choosed_functions, availa
         trigonometric_labels.append(col_name + "_arg")
 
     for choosed_function, function in zip(choosed_functions, available_functions):
-        if choosed_function :
-            trigonometric_labels.append(col_name+"_"+function)
+        if choosed_function:
+            trigonometric_labels.append(col_name + "_" + function)
     return trigonometric_labels
+
 
 def apply_trigonometric_transform(x, choosed_functions, output_arg):
     """
@@ -149,19 +152,19 @@ def apply_trigonometric_transform(x, choosed_functions, output_arg):
         x = int(x)
         arg_val = trigonometric_transformer.compute_argument(x)
         if process_as_time_dimension:
-            cos_val = trigonometric_features['cos'][x]
-            sin_val = trigonometric_features['sin'][x]
-            tan_val = trigonometric_features['tan'][x]
+            cos_val = trigonometric_features["cos"][x]
+            sin_val = trigonometric_features["sin"][x]
+            tan_val = trigonometric_features["tan"][x]
         else:
             cos_val, sin_val, tan_val = trigonometric_transformer.compute_trigonometric_transform(arg_val)
         log_message = None
 
     except Exception as e:
-        log_message = "ERROR : "+str(e)
-        arg_val, cos_val, sin_val, tan_val  = None, None, None, None
+        log_message = "ERROR : " + str(e)
+        arg_val, cos_val, sin_val, tan_val = None, None, None, None
     trig_results = [cos_val, sin_val, tan_val]
 
-    if output_arg :
+    if output_arg:
         periodic_features.append(arg_val)
     for choice, trig_result in zip(choosed_functions, trig_results):
         if choice:
@@ -169,39 +172,43 @@ def apply_trigonometric_transform(x, choosed_functions, output_arg):
 
     return periodic_features, log_message
 
-#Parameters import :
-periodical_cols = params.get('periodical_columns')
-process_as_time_dimension = params.get('process_as_time_dimension')
-column_period = params.get('column_period')
-compute_cos = params.get('compute_cos')
-compute_sin = params.get('compute_sin')
-compute_tan = params.get('compute_tan')
-output_arg = params.get('output_arg')
 
-#A variable allowing the boolean mapping to the functions to use :
+# Parameters import :
+periodical_cols = params.get("periodical_columns")
+process_as_time_dimension = params.get("process_as_time_dimension")
+column_period = params.get("column_period")
+compute_cos = params.get("compute_cos")
+compute_sin = params.get("compute_sin")
+compute_tan = params.get("compute_tan")
+output_arg = params.get("output_arg")
+
+# A variable allowing the boolean mapping to the functions to use :
 choosed_functions = [compute_cos, compute_sin, compute_tan]
-#Constant:
-AVAILABLE_FUNCTIONS = ['cos', 'sin', 'tan']
+# Constant:
+AVAILABLE_FUNCTIONS = ["cos", "sin", "tan"]
 
-#Variable allowing to label the created columns :
+# Variable allowing to label the created columns :
 trigonometric_mapping = {}
-for col_name in periodical_cols :
-    trigonometric_mapping[col_name] = compute_trigonometric_labels(col_name, output_arg, choosed_functions, AVAILABLE_FUNCTIONS)
+for col_name in periodical_cols:
+    trigonometric_mapping[col_name] = compute_trigonometric_labels(
+        col_name, output_arg, choosed_functions, AVAILABLE_FUNCTIONS
+    )
 
-#Trigonometric transformer class instantiation :
-trigonometric_transformer = PeriodicalFeaturesTransformer(choosed_functions=choosed_functions,
-                                                     period=column_period,
-                                                     process_as_time_dimension=process_as_time_dimension)
+# Trigonometric transformer class instantiation :
+trigonometric_transformer = PeriodicalFeaturesTransformer(
+    choosed_functions=choosed_functions, period=column_period, process_as_time_dimension=process_as_time_dimension
+)
 
 trigonometric_features = trigonometric_transformer.trigonometric_features
 
-#Production code :
+
+# Production code :
 def process(row):
     for col_name in periodical_cols:
         trigonometric_labels = trigonometric_mapping[col_name]
         raw_value = row[col_name]
         periodic_features, log_message = apply_trigonometric_transform(raw_value, choosed_functions, output_arg)
-        row['trigonometric_log_%s'%col_name] = log_message
+        row["trigonometric_log_%s" % col_name] = log_message
         for label, feature in zip(trigonometric_labels, periodic_features):
             row[label] = feature
     return row
